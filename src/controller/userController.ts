5fb58617a2c5dcc0613e1cec7cae30129b1ad0e0
Luam-Novais <luampabl014@gmail.com>
2025-10-27 22:28:47 -0300

    feat: implementar metodo para login do usuario

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -1,14 +1,19 @@
 import type { Request, Response } from 'express';
 import { UserRepository } from '../repository/userRepository.js';
 import { UserService } from '../service/userService.js';
-import type { IUser } from '../types/User.js';
+import type { IUser, userCredentials } from '../types/User.js';
 
 const userRepository = new UserRepository();
 const userService = new UserService(userRepository);
 
 export class UserController {
   async login(req: Request, res: Response) {
-    res.status(200).json(userService.login());
+    const { email, password } = req.body;
+    if (!email || !password) res.status(400).json('Por favor nos informe os dados necessários.');
+    const userCredentials: userCredentials = { email, password };
+    const loginUser = await userService.login(userCredentials);
+    if (loginUser instanceof Error) res.status(401).json(loginUser.message);
+    res.status(200).json(loginUser);
   }
   async register(req: Request<{}, {}, IUser>, res: Response) {
     const { name, email, phone, address, password, cep } = req.body;
@@ -19,11 +24,11 @@ export class UserController {
         phone,
         address,
         password,
-        cep
+        cep,
       };
       const registerUser = await userService.register(user);
-      if(registerUser instanceof Error) res.status(400).json(registerUser.message)
-      res.status(201).json(registerUser)
+      if (registerUser instanceof Error) res.status(400).json(registerUser.message);
+      res.status(201).json(registerUser);
     }
   }
 }
