a0cf8e087fd30b752d6efc01b7c6b7c4fea4a991
Luam-Novais <luampabl014@gmail.com>
2025-10-27 22:31:05 -0300

    feat: adicionar rota para login

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -8,7 +8,7 @@ import { verifyCepMiddleware } from '../middleware/verifyCepMiddleware.js';
 const router = Router();
 const controller = new UserController();
 
-router.get('/login', (req, res) => controller.login(req, res));
+router.post('/login', (req, res) => controller.login(req, res));
 router.post('/register',errorRegexMiddleware,  verifyCepMiddleware,(req : Request, res : Response) => controller.register(req, res));
 
 export default router;
