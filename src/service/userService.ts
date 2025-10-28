21eeb48cae6e7b1b108324bcb92e668d7a8b26a0
Luam-Novais <luampabl014@gmail.com>
2025-10-27 22:33:38 -0300

    feat: implementar metodo para login do usuario

--- src/service/userService.ts
+++ src/service/userService.ts
@@ -1,7 +1,7 @@
-import type { IUser } from '../types/User.js';
+import type { IUser, userCredentials } from '../types/User.js';
 import { UserRepository } from '../repository/userRepository.js';
 import { FormatedString } from '../utils/formatedString.js';
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import { ErrorHandlerHttp } from '../error/errorHandlerHttp.js';
 
@@ -9,9 +9,23 @@ const { formatString } = new FormatedString();
 
 export class UserService {
   constructor(private userRepository: UserRepository) {}
-  async login() {}
-  async register(user: IUser): Promise<IUser | object | Error> {
 
+  async login(user: userCredentials) {
+    const userExisting = await this.userRepository.findByEmail(user.email);
+    if (userExisting) {
+      const comparePassword = bcrypt.compareSync(user.password, userExisting.password)
+      if(!comparePassword) return new ErrorHandlerHttp(400, 'Credenciais inválidas.');
+      const payload = {
+        id: userExisting.id,
+        name: userExisting.name
+      }
+      const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, {expiresIn: '1h'})
+      return { token}
+    } else {
+      return new ErrorHandlerHttp(400, 'Credenciais inválidas.');
+    }
+  }
+  async register(user: IUser): Promise<IUser | object | Error> {
     const userFormated: IUser = {
       name: formatString(user.name),
       email: formatString(user.email),
@@ -21,15 +35,14 @@ export class UserService {
       password: bcrypt.hashSync(user.password, 10),
     };
     const registeredUser = await this.userRepository.register(userFormated);
-    if(registeredUser instanceof Error) return registeredUser
-    else{
+    if (registeredUser instanceof Error) return registeredUser;
+    else {
       const payload = {
         id: registeredUser.id,
-        name: registeredUser.name
-      }
-      if(!process.env.JWT_SECRET_KEY) return new ErrorHandlerHttp(400, 'Secret Key is undefined.')
+        name: registeredUser.name,
+      };
       const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn: '1h' });
-      return {registeredUser, token}
+      return { registeredUser, token };
     }
   }
 }
