07f93ff978cb61c7730ae6a963f1304b19c5ff9b
Luam-Novais <luampabl014@gmail.com>
2025-10-30 20:14:47 -0300

    refactor: refatorar codigo para deixa-lo mais limpo e mais escalavel

--- src/middleware/errorRegexMiddleware.ts
+++ src/middleware/errorRegexMiddleware.ts
@@ -1,24 +1,20 @@
 import type { Request, Response, NextFunction } from 'express';
 import type { IUser } from '../types/User.js';
 import { ValidadeRegex } from '../utils/validateRegex.js';
-import { FormatedString } from '../utils/formatedString.js';
+import type { regexKey } from '../utils/validateRegex.js';
+import { ErrorHandlerHttp } from '../error/errorHandlerHttp.js';
 
 const { validate } = new ValidadeRegex();
 
-
 export async function errorRegexMiddleware(req: Request<{}, {}, IUser>, res: Response, next: NextFunction) {
-  const { email, password, phone, cep } = req.body;
-  const emailValidation = validate('email', email);
-  const passwordValidation = validate('password', password);
-  const phoneValidation = validate('phone', phone);
-  const cepValidation = validate('cep', cep)
+  const fieldsToValidate = ['email', 'password', 'phone', 'cep'];
+  for (const field of fieldsToValidate) {
+    const valueToValidate: string = req.body[field as regexKey];
+    if (!valueToValidate) continue;
+    const fieldValidate = validate(field as regexKey, valueToValidate);
+    if (fieldValidate instanceof Error) {
+      return res.status(400).json({field, fieldValidate: fieldValidate.message})
+    }
+  } next();
 
-
-  if (emailValidation instanceof Error) res.status(400).json(emailValidation.message);
-  else if (passwordValidation instanceof Error) res.status(400).json(passwordValidation.message);
-  else if (phoneValidation instanceof Error) res.status(400).json(phoneValidation.message);
-  else if (cepValidation instanceof Error) res.status(400).json(cepValidation.message);
-  else {
-    next();
-  }
 }
