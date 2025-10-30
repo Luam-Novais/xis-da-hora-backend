a5ae314e01d54d877035ad94a08101473e72479a
Luam-Novais <luampabl014@gmail.com>
2025-10-30 20:16:22 -0300

    feat: implementar middleware para validacao de cep, na rota de criar usuarios

--- src/middleware/verifyCepMiddleware.ts
+++ src/middleware/verifyCepMiddleware.ts
@@ -1,14 +1,14 @@
-import type { Request, Response, NextFunction } from "express";
-import { FormatedString } from "../utils/formatedString.js";
-const { formatString } = new FormatedString();
-export  async function verifyCepMiddleware(req: Request, res: Response, next: NextFunction){
-    const {cep} = req.body
-    const cepFormated = formatString(cep).replace('-', '').replace('.', '');
-    try{
-        const response = await fetch(`https://viacep.com.br/ws/${cepFormated}/json/`);
-        if(response.ok) next();
-    }catch(error){
-        console.log(error)
-        res.status(400).json('Ocorreu um erro ao consumir api viacep')
-    }
+import type { Request, Response, NextFunction } from 'express';
+import { FormaterString } from '../utils/formaterString.js';
+const { formatString } = new FormaterString();
+export async function verifyCepMiddleware(req: Request, res: Response, next: NextFunction) {
+  const { cep } = req.body;
+  const cepFormated = formatString(cep).replace('-', '').replace('.', '');
+  try {
+    const response = await fetch(`https://viacep.com.br/ws/${cepFormated}/json/`);
+    if (response.ok) next();
+  } catch (error) {
+    console.log(error);
+    res.status(400).json('Ocorreu um erro ao consumir api viacep');
+  }
 }
