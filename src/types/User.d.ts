3493f5afdca891445259d15edeb39f0e4eaafdec
Luam-Novais <luampabl014@gmail.com>
2025-10-30 20:28:22 -0300

    feat: adicionar interface para usuario editado

--- src/types/User.d.ts
+++ src/types/User.d.ts
@@ -7,7 +7,19 @@ export interface IUser {
     phone: string
     cep: string
 }
-export interface userCredentials {
+export interface IEditUser {
+  id: number;
+  name?: string;
+  email?: string;
+  password?: string;
+  address?: string;
+  phone?: string;
+  cep?: string;
+}
+export interface IUserCredentials {
     email: string,
     password: string
+}
+export interface IUserPayload{
+    id: number
 }
