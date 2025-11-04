f3b766a053f362c2d41972ecb5ee26254dde1179
Luam-Novais <luampabl014@gmail.com>
2025-11-04 19:00:44 -0300

    feat: adicionar middleware para as rotas de produto

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import userRouter from './routes/userRouter.js'
+import productRouter from './routes/productRoutes.js'
 
 
 dotenv.config()
@@ -8,5 +9,6 @@ const app = express()
 app.use(express.urlencoded())
 app.use(express.json())
 app.use('/user', userRouter)
+app.use('/product', productRouter)
 
 app.listen(3000, ()=> console.log('Servidor rodando...'))
