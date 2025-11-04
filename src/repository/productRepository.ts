fc4b727df6328668975149e908eec7933ea874b8
Luam-Novais <luampabl014@gmail.com>
2025-11-04 18:55:43 -0300

    feat: adicionar repositório de produtos com métodos para buscar categorias e produtos por categoria

---
+++ src/repository/productRepository.ts
@@ -0,0 +1,18 @@
+import type { Category, Product } from "@prisma/client";
+import prisma from "../config/prisma.js";
+import type { IProduct } from "../types/product.js";
+import { ErrorHandlerHttp } from "../error/errorHandlerHttp.js";
+
+export class ProductRepository {
+  async findCategory(category: string): Promise<Category | null> {
+    return await prisma.category.findUnique({ where: { name: category } });
+  }
+  async getByCategory(category: string): Promise<IProduct[] | Error> {
+    const categoryExisting = await this.findCategory(category)
+    if(!categoryExisting) return new Error('Categoria inválida.')
+    const products = await prisma.product.findMany({
+      where: { categoryName: category },
+    });
+    return products
+  }
+}
