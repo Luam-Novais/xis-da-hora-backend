cc18efead187e5c609ece1ff3dde6875e41dfe4a
Luam-Novais <luampabl014@gmail.com>
2025-11-04 18:54:08 -0300

    feat: implementar controle de rotas para os produtos

---
+++ src/controller/productController.ts
@@ -0,0 +1,19 @@
+import type { Request, Response } from 'express';
+import { ProductRepository } from '../repository/productRepository.js';
+import { ProductService } from '../service/productService.js';
+import { ErrorHandlerHttp } from '../error/errorHandlerHttp.js';
+
+const productRepository = new ProductRepository();
+const productService = new ProductService(productRepository);
+export class ProductController {
+  async getAllProducts(req: Request, res: Response) {
+    console.log('testeeeee');
+  }
+  async getProductsByCategory(req: Request, res: Response) {
+    const { category } = req.query;
+    if (typeof category !== 'string') return res.status(400).json({ message: 'Query inválida.' });
+    const products = await productService.getProductsByCategory(category);
+    if(products instanceof Error) res.status(400).json(products.message)
+    res.status(200).json(products)
+  }
+}
