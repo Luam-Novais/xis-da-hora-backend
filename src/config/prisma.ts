f7e4ba599e9aec18ba359ee680404ec5b677423d
Luam-Novais <luampabl014@gmail.com>
2025-12-01 20:52:01 -0300

    chore: update Prisma and dependencies, refactor Prisma client configuration
    - Updated Prisma and @prisma/client to version 7.0.1 in package.json.
    - Added @prisma/adapter-pg to package.json for PostgreSQL support.
    - Removed the engine configuration from prisma.config.ts.
    - Refactored Prisma client initialization in src/config/prisma.ts to use the new adapter.
    - Imported dotenv configuration in src/index.ts for environment variable management.

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,9 +1,9 @@
 import { PrismaClient } from "@prisma/client";
-import { url } from "inspector";
+import { PrismaPg } from '@prisma/adapter-pg'; // pacote de exemplo
 
-const config = {
-  url: process.env.DATABASE_URL,
-};
-const prisma = new PrismaClient()
-
+const prisma = new PrismaClient({
+  adapter: new PrismaPg({
+    connectionString: process.env.DATABASE_URL,
+  }),
+});
 export default prisma
