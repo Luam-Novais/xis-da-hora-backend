cc73de6afefa4b24d2ed43ffb4fa53e7893d6585
Luam-Novais <luampabl014@gmail.com>
2025-12-03 22:25:39 -0300

    refactor: classe para lidar com erros

--- src/error/httpError.ts
+++ src/error/httpError.ts
@@ -1,6 +1,7 @@
+
 export class HttpError extends Error{
     status: number
-    constructor(status: number, error: string){
+    constructor(status : number, error: string){
         super(error)
         this.status = status
     }
